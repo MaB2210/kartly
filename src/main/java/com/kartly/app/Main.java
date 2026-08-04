24ad26f526c859652758486e39fb28baca26e6a7
MaB2210 <connect.mannbhatt@gmail.com>
2026-08-04 15:24:48 -0400

    chore: add sanity-check main class

---
+++ src/main/java/com/kartly/app/Main.java
@@ -0,0 +1,7 @@
+package com.kartly.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Kartly bootstraped successfully - Java" + System.getProperty("java.version"));
+    }
+}
